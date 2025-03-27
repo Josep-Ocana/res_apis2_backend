e93290cad1fc09fe8782c962420638697cfc2077
Josep Ocaña <j.ocanapuigdevall@gmail.com>
2025-03-27 16:42:16 +0100

    Fix: Añadido SSL en la conexión a PostgreSQL

--- src/config/db.ts
+++ src/config/db.ts
@@ -7,9 +7,14 @@ console.log()
 // Añadimos ! al final garantizas que el valor estará y no será UNDEFINED
 const db = new Sequelize(process.env.DATABASE_URL!,{
     models: [__dirname + '/../models/**/*'],
-    
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    logging: false   
 })
-//hola
-
 
 export default db
